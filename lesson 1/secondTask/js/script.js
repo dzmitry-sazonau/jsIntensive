e76678a4481c78f2d576a6064276569105227629
Dima <mister.mitya99@gmail.com>
2018-10-07 14:20:42 +0300

    Изменил на первые цифры

--- lesson 1/secondTask/js/script.js
+++ lesson 1/secondTask/js/script.js
@@ -3,6 +3,7 @@
 let num = prompt('Введите число', '33721'),
     pow = prompt('Введите степень', '3'),
     arr = [],
+    arrOp = [],
     op = 1;
 
 for(let i = 0; i < num.length; i++){
@@ -19,6 +20,16 @@ for(let i = 1; i < pow; i++){
     powOp *= op;
 }
 
+// let powOpString = powOp.toString(),
+//     lengthNum = (powOpString.length)*10;
+
+// console.log(lengthNum);
 console.log("Наше число -", op);
-console.log('Число в', pow,'степени', powOp);
-console.log('Последние 2 цифры', powOp%100);
+console.log('Число в', pow,'степени -', powOp);
+let powOpString = powOp.toString();
+
+for(let i = 0; i < powOpString.length; i++){
+    arrOp[i] = +powOpString[i];
+}
+
+console.log('Это первые две цифры -', arrOp[0], arrOp[1]);
