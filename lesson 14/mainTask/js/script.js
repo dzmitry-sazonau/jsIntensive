d521118bfb84a2b238e7b021a68971ec99418050
Dima <mister.mitya99@gmail.com>
2018-10-29 15:22:31 +0300

    fixed

--- lesson 14/mainTask/js/script.js
+++ lesson 14/mainTask/js/script.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
     
-    $('.main_btn').on('click', function() {
-        viewModalWindow()
-    });
-
-    $('.main_btna').on('click', function() {
-        viewModalWindow();
-    });
-
-    $('#viewModal').on('click', function() {
-        viewModalWindow();
+    $('.main_btna, .main_btn, li [href = \\#sheldure]').on('click', function() {
+        $('.overlay').animate({opacity: 'toggle',}, 2000);
+        $('.modal').animate({height: "show", }, 2000);
     });
 
     $('.close').on('click', function() {
         $('.overlay').animate({opacity: 'hide',}, 1000);
         $('.modal').animate({height: "hide", }, 1000);
     });
-
-    function viewModalWindow() {
-        $('.overlay').animate({opacity: 'toggle',}, 2000);
-        $('.modal').animate({height: "show", }, 2000);
-    }
 });
